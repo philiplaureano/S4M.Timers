a63cc9e4b0bdfd0ed04b8006c8398e8087d4db74
Philip Laureano <philip.laureano@gmail.com>
2021-03-07 22:45:42 +1100

    Added the test stubs for the scheduler tests

---
+++ S4M.Timers/S4M.Timers.Tests/SchedulerTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace S4M.Timers.Tests
+{
+    public class SchedulerTests
+    {
+        [Fact(DisplayName = "We should be able to schedule a message to be told once")]
+        public async Task ShouldBeAbleToScheduleATellOnce()
+        {
+            throw new NotImplementedException("TODO: Implement the ShouldBeAbleToScheduleATellOnce test");
+        }
+
+        [Fact(DisplayName = "We should be able to schedule a message to be told repeatedly")]
+        public async Task ShouldBeAbleToScheduleARepeatedTell()
+        {
+            throw new NotImplementedException("TODO: Implement the ShouldBeAbleToScheduleARepeatedTell test");
+        }
+    }
+}
